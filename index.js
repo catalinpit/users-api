310ffbca64630c23a1b9c6cb2106353f93b63213
Catalin Pit <catalinpit@gmail.com>
2020-09-21 16:51:31 +0300

    Made a few changes

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || "8888";
+const port = process.env.PORT || '8888';
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.status(200).send("sss");
+    res.render('homepage', { title: 'Homepage' });
 });
 
 app.listen(port, () => {
