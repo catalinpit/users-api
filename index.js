aa7f70dba2f3b6e6e50be1a5eef0b9daa79cf0d0
Catalin Pit <catalinpit@192-168-0-235.rdsnet.ro>
2020-09-24 15:13:18 +0300

    wip

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const path = require('path');
+const usersRoute = require('./routes/users');
+
+require('dotenv/config');
 
 const app = express();
 const port = process.env.PORT || '8888';
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'pug'); 
 
-app.get('/', (req, res) => {
-    res.render('homepage', { title: 'Homepage' });
+app.use('/', usersRoute);
+
+app.get("/login", function(req, res) {
+    res.render("login", { title: 'Login Page' });
 });
 
 app.listen(port, () => {
